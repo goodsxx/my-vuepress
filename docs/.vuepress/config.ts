22105f4ebf3b3d0c42f8ec8b1be83a0f8fb8cf72
sxx <1368084801@qq.com>
2022-04-18 11:33:19 +0800

    测试

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,53 @@
+import { defineUserConfig } from 'vuepress-vite'
+import type { DefaultThemeOptions } from 'vuepress-vite'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  // 站点配置
+  lang: 'zh-CN',
+  title: '宋鑫鑫的学习笔记',
+  description: 'Just do it!',
+
+  // 主题和它的配置
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: '/images/s.png',
+    //导航栏
+    navbar: [
+      // 控制元素何时被激活
+      {
+        text: '前端',
+        children: [
+          {
+            text: 'vue.md',
+            link: '/前端/vue.md',
+            // 该元素将一直处于激活状态
+            activeMatch: '/前端/vue.md',
+          },
+        ],
+      },
+      {
+        text: '后端',
+        children: [
+          {
+            text: 'Asp.Net.md',
+            link: '/后端/Asp.Net.md',
+            // 该元素将一直处于激活状态
+            activeMatch: '/后端/Asp.Net.md',
+          },
+        ],
+      },
+      // 嵌套 Group - 最大深度为 2
+      {
+        text: '其他',
+        children: [
+          {
+            text: '笔记',
+            children: ['/其他/笔记/笔记.md'],
+          },
+        ],
+      }
+    ],
+    //仓库地址
+    repo: 'https://gitee.com/goodsxx/my-vuepress',
+  },
+})
