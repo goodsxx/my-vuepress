0f97ca2750834cf370d2e2b25e7f6825ca4e8488
SongXinXin <1368084801@qq.com>
2022-04-19 10:44:03 +0800

    脚本修改

--- start.sh
+++ start.sh
@@ -6,12 +6,10 @@ cd /home/my/my-vuepress
 git pull
 
 # 杀死目前已启动进程
-ID=`ps -ef|grep node |grep ScriptExecutor | grep vuepress|awk '{print $2}'`
+ID=`ps -ef|grep node | grep vuepress|awk '{print $2}'`
 echo --- the process is $ID ---
 kill -9  $ID
 echo  "Killed $ID"
 
 # 启动
 nohup yarn pro&
-# 启动webhookAPI
-nohup dotnet /home/my/script-executor/ScriptExecutor.dll&
