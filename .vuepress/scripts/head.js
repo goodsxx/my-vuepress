f4b585d578ce54801dd762cc9521b0460c8efde3
SongXinXin <1368084801@qq.com>
2022-04-21 14:00:30 +0800

    新增 SEO优化插件

--- .vuepress/scripts/head.js
+++ .vuepress/scripts/head.js
@@ -2,36 +2,43 @@ module.exports = [
     [
         "meta",//百度站长验证
         {
-        name: "baidu-site-verification",
-        content: "code-GRkx78Unk8"
+            name: "baidu-site-verification",
+            content: "code-GRkx78Unk8"
         }
     ],
     [
         "meta",//Bing站长验证
         {
-        name: "msvalidate.01",
-        content: "B203CBD1576DBD14E51A9B4AAD85386C"
+            name: "msvalidate.01",
+            content: "B203CBD1576DBD14E51A9B4AAD85386C"
         }
     ],
     [
         "meta",//Google站长验证
         {
-        name: "google-site-verification",
-        content: "qxA3JVZhNxhf9pq5_quneWi6N4WCugQ-3oy3fI6TuuU"
+            name: "google-site-verification",
+            content: "qxA3JVZhNxhf9pq5_quneWi6N4WCugQ-3oy3fI6TuuU"
         }
     ],
     [
         "meta",//移动端优化
         {
-        name: "viewport",
-        content: "width=device-width,initial-scale=1,user-scalable=no"
+            equiv: "X-UA-Compatible",
+            content: "IE=edge"
+        }
+    ],
+    [
+        "meta",//移动端优化
+        {
+            name: "viewport",
+            content: "width=device-width,initial-scale=1,user-scalable=no"
         }
     ],
     [
         "link",
         {
-        rel: "icon",
-        href: "/images/s.png"
+            rel: "icon",
+            href: "/images/s.png"
         }
     ],
 ]
--- .vuepress/scripts/plugins.js
+++ .vuepress/scripts/plugins.js
@@ -1,32 +1,23 @@
 //插件
 module.exports=[
-    // //音乐播放器
-    // 'meting',{
-    //     meting: {
-    //         // 歌单地址-> 如果输入可忽略server|type|mid
-    //         // 但是不知道为什么不写上这三个会报错, 所以我都写上了
-    //         auto: 'https://music.163.com/playlist?id=462299348',
-    //         // 当前服务为netease -> 网易
-    //         server: "netease",
-    //         // 类型为歌单
-    //         type: "playlist",
-    //         // 歌单id
-    //         mid: "462299348",
-    //       },
-    //       aplayer: {
-    //         lrcType: 3,
-    //      },
-        //   aplayer: {
-        //     // 歌单为随机
-        //     order: 'random',
-        //     // 0为不显示歌词
-        //     lrcType: 0,
-        //     // 音量
-        //     volume: 0.15,
-        //     // 开启迷你模式
-        //     mini: true,
-        //     // 自动播放
-        //     autoplay: true
-        //   },
-    // },
+    //meta优化 https://github.com/webmasterish/vuepress-plugin-autometa
+    'autometa', {
+        site: {
+          name: 'Bravo Yeung',
+          twitter: 'yanglr',
+        },
+        canonical_base: 'https://dbdgs.cn',
+    },
+    //添加sitemap https://github.com/ekoeryanto/vuepress-plugin-sitemap
+    'sitemap', {
+        hostname: "https://dbdgs.cn",
+        // 排除无实际内容的页面
+        exclude: ["/404.html"]
+    },
+    //增加feed https://github.com/webmasterish/vuepress-plugin-feed
+    'feed',{
+        canonical_base: 'https://webmasterish.com'
+    },
+    //自动推送到百度 https://github.com/IOriens/vuepress-plugin-baidu-autopush
+    'vuepress-plugin-baidu-autopush',
 ]
