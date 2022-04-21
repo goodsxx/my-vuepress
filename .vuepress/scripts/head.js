c0b309113a01007d2480b46d0898605de4a9dd52
SongXinXin <1368084801@qq.com>
2022-04-21 11:49:03 +0800

    添加百度站长验证

--- .vuepress/scripts/head.js
+++ .vuepress/scripts/head.js
@@ -12,5 +12,12 @@ module.exports = [
         name: "viewport",
         content: "width=device-width,initial-scale=1,user-scalable=no"
         }
+    ],
+    [
+        "meta",//百度站长验证
+        {
+        name: "baidu-site-verification",
+        content: "code-3DB9r3sWXy"
+        }
     ]
 ]
